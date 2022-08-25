da3ee9e6a0ace4e561c126a5b63266fd54d56968
Mulate Eyoel <s2200125@BC-C02GR0BRQ6LR-MacBook-Air.local>
2022-08-25 14:05:32 +0300

    first commit

---
+++ src/Controller/SecondController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\second_module\Controller;
+
+class SecondController {
+    
+    public function second() {
+        
+        return array('#markup' => 'Second module is coming soon');
+    }
+}
